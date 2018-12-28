eab038ed9882453921d1e2a8498e56f28dddc8cd
Tracy Nguyen <tracy.nguyen@richmond.edu>
2018-12-28 00:09:48 -0500

    Deploying static ACM website

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+# This file only serves to help deploy the site on Heroku
+# Source: https://medium.com/@adityaniloi/how-to-deploy-a-static-website-to-heroku-49d55e07cb94
+include_once("home.html"); ?>
