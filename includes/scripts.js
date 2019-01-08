582e136b5fe53fca306d49f35d1f116a48404c29
Tracy Nguyen <tracy.nguyen@richmond.edu>
2019-01-08 02:18:39 -0500

    testing

--- includes/scripts.js
+++ includes/scripts.js
@@ -9,9 +9,11 @@ function getLevel(){
 		case "1": return "";
 		case "2": return "../../";
 		case "3": return "../../../";
-		case "4": return "../../../";
+		case "4": return "../../../../";
+		default: 
+			alert($('meta[name=level]').attr("content"));
+			return "";
 	}
-	alert($('meta[name=level]').attr("content"));
 }
 
 function setHeader(dir) {
